526b8884428e8443bb18eb550a7aa5e802ebe576
sergio-incaser <sergio@incaser.es>
2015-11-25 19:31:00 +0100

    [DEV] left_menu_structure: Develop.

---
+++ website_product_supplier_left_menu/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Website Product Supplier Left Menu",
+    'summary': "New layout to show products options into left menu",
+    'category': 'Website',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'website_product_supplier',
+        'website_portal_left_menu',
+    ],
+    'data': [
+        'views/website_product_supplier.xml',
+        'views/website_portal_left_menu_layout.xml',
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Incaser Informatica S.L., ',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
